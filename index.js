6f49aca1716fcaf8f10f4666ac47550b8b75e5e3
GUICHARD Pierre <pierre.guichard77@gmail.com>
2021-06-26 19:31:42 +0200

    add mongoose

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
+const dotenv = require('dotenv');
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
 
-app.use(express.urlencoded({ extended: true }));
+dotenv.config();
 
+//connection to db
+mongoose.set('useFindAndModify', false);
 
-// Routing requests :
-app.use('/api/task', require('./src/routes/task'));
-
-
-// Launch server
-app.listen(3000, () => console.log("Server running"));
+mongoose
+    .connect(
+        process.env.DB_CONNECT, { 
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        })
+    .then( () => {
+        app.use(express.urlencoded({ extended: true }));
+        
+        // Routing requests :
+        app.use('/api/task', require('./src/routes/task'));
+        
+        const port = process.env.API_PORT ? process.env.API_PORT : 3000;
+        // Launch server
+        app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
+    })
